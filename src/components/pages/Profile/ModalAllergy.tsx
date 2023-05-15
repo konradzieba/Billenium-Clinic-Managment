5c3bbf3f4189993736f86fd04b084f3d4cc9063e
Konrad Zięba <162624@student.uwm.edu.pl>
2023-05-15 21:21:17 +0200

    Improve ProfileInfo and dedicated modals

--- src/components/pages/Profile/ModalAllergy.tsx
+++ src/components/pages/Profile/ModalAllergy.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import { Dispatch, useState } from 'react';
 import { Modal, MultiSelect, Flex, Button } from '@mantine/core';
 
 type ModalAllergyProps = {
-    opened:boolean;
-    setOpen:React.Dispatch<boolean>;
-}
+  opened: boolean;
+  setOpen: Dispatch<boolean>;
+};
 const alergie = [
   'powietrze',
   'woda',
@@ -15,37 +15,43 @@ const alergie = [
   'zimny makaron',
   'masło',
   'szynka',
-  'kartofle'
-]
+  'kartofle',
+];
 
-const ModalAllergy = (props:ModalAllergyProps) => {
-  return(
+const ModalAllergy = (props: ModalAllergyProps) => {
+  const [dropdownOpened, setDropdownOpened] = useState(false);
+  return (
     <Modal
-      radius='md'
+      radius="md"
       opened={props.opened}
       onClose={() => props.setOpen(false)}
       title="Dodaj alergię"
+      zIndex={9999}
     >
       <Flex
-        h='300px'
+        h={dropdownOpened ? '280px' : 'auto'}
         direction={'column'}
         align={'center'}
         justify={'space-between'}
       >
         <MultiSelect
-          w='100%'
+          onDropdownOpen={() => setDropdownOpened(true)}
+          onDropdownClose={() => setDropdownOpened(false)}
+          w="100%"
           data={alergie}
           label="Wpisz swoje alregie"
           placeholder="Nazwa alergii"
           getCreateLabel={(query) => `+ Dodaj ${query}`}
           searchable
           creatable
-          maxDropdownHeight={200}
+          maxDropdownHeight={120}
         />
-        <Button mt='md' variant='outline'>Dodaj alergię</Button>
+        <Button mt="md" variant="outline">
+          Dodaj alergię
+        </Button>
       </Flex>
     </Modal>
-  )
-}
+  );
+};
 
-export default ModalAllergy
+export default ModalAllergy;
--- src/components/pages/Profile/ModalMedicines.tsx
+++ src/components/pages/Profile/ModalMedicines.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import { Dispatch } from 'react';
 import { useState } from 'react';
 import { Button, Flex, Modal, Select, Autocomplete } from '@mantine/core';
 
 type ModalMedicinesProps = {
-    opened:boolean;
-    setOpen:React.Dispatch<boolean>;
-}
+  opened: boolean;
+  setOpen: Dispatch<boolean>;
+};
 
 const leki = [
   'Paracetamol',
@@ -27,68 +27,67 @@ const leki = [
   'Mucosolvan',
   'Coldrex',
   'Rutinoscorbin',
-  'Doppelherz'
+  'Doppelherz',
 ];
 
-const reg = new RegExp(/^\d+$/);
-const dawki_lekow = ['mg', 'g', 'ug', 'ng', 'szt']
-const ModalMedicines = (props:ModalMedicinesProps) => {
+const autoCompleteRegex = new RegExp(/^\d+$/);
+const dawki_lekow = ['mg', 'g', 'ug', 'ng', 'szt.'];
+const ModalMedicines = (props: ModalMedicinesProps) => {
   const [data, setData] = useState(leki);
   const [open, setOpen] = useState(false);
   const [open2, setOpen2] = useState(false);
 
   const [value, setValue] = useState('');
   const dawki =
-    value.trim().length > 0 && value.match(reg)
+    value.trim().length > 0 && value.match(autoCompleteRegex)
       ? dawki_lekow.map((provider) => `${value} ${provider}`)
       : [];
-  return(
+  return (
     <Modal
-      radius='md'
+      radius="md"
       opened={props.opened}
       onClose={() => props.setOpen(false)}
       title="Dodaj lekarstwo"
-      size='md'
+      size="md"
+      zIndex={9999}
     >
-      <Flex
-        h={open || open2 ? '200px' : '80px'}
-      >
-        <Flex>
-        <Select
-          onDropdownOpen={() => setOpen(true)}
-          onDropdownClose={() => setOpen(false)}
-          w='80%'
-          data={data}
-          label="Wpisz swoje lekarstwa"
-          placeholder="Nazwa lekarstwa"
-          searchable
-          getCreateLabel={(query) => `+ Dodaj ${query}`}
-          onCreate={(query) => {
-            const item = query ;
-            setData((current) => [...current, item]);
-            return item;
-          }}
-          creatable
-          nothingFound="Brak leku"
-          maxDropdownHeight={120}
-        />
-        <Autocomplete
-          onDropdownOpen={() => setOpen2(true)}
-          onDropdownClose={() => setOpen2(false)}
-          w='30%'
-          data={dawki}
-          value={value}
-          onChange={setValue}
-          label="Dawka"
-          placeholder="Dawka"
-          maxDropdownHeight={120}
-        />
+      <Flex h={open || open2 ? '200px' : '80px'}>
+        <Flex gap="sm">
+          <Select
+            onDropdownOpen={() => setOpen(true)}
+            onDropdownClose={() => setOpen(false)}
+            w="80%"
+            data={data}
+            label="Wpisz swoje lekarstwa"
+            placeholder="Nazwa lekarstwa"
+            searchable
+            getCreateLabel={(query) => `+ Dodaj ${query}`}
+            onCreate={(query) => {
+              const item = query;
+              setData((current) => [...current, item]);
+              return item;
+            }}
+            creatable
+            maxDropdownHeight={120}
+          />
+          <Autocomplete
+            onDropdownOpen={() => setOpen2(true)}
+            onDropdownClose={() => setOpen2(false)}
+            w="30%"
+            data={dawki}
+            value={value}
+            onChange={setValue}
+            label="Dawka"
+            placeholder="Dawka"
+            maxDropdownHeight={120}
+          />
         </Flex>
-
       </Flex>
-      <Button mt='md' variant='outline'>Dodaj lekarstwo</Button>
+      <Button mt="md" variant="outline">
+        Dodaj lekarstwo
+      </Button>
     </Modal>
-  )
-}
+  );
+};
 
-export default ModalMedicines
+export default ModalMedicines;

4ffe9e92e2687c4077b7231fd959bef9950d15e4
ArkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2023-06-01 18:12:44 +0200

    Add routing to specializations tab

--- src/components/pages/Doctors/DoctorsList.tsx
+++ src/components/pages/Doctors/DoctorsList.tsx
@@ -1,10 +1,11 @@
-import { Box, Flex, Loader, Title } from '@mantine/core';
+import { Box, Button, Flex, Loader, Title } from '@mantine/core';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 import { DoctorListType } from '../../../helpers/types';
 import AppointmentStepper from '../../UI/AppointmentStepper';
 import Doctor from './Doctor';
+import { useNavigate } from 'react-router-dom';
 
 type DoctorListProps = {
   specialization: string;
@@ -13,6 +14,7 @@ const URL =
   'http://localhost:8080/api/doctors/by-specialization?specialization=';
 
 export const DoctorList = ({ specialization }: DoctorListProps) => {
+  const navigate = useNavigate();
   const fetchDoctors = async (specialization: string) => {
     const response = await axios.get(`${URL}${specialization}`);
     return response.data as DoctorListType[];
@@ -45,6 +47,13 @@ export const DoctorList = ({ specialization }: DoctorListProps) => {
           );
         })}
       </Box>
+      <Button
+        ml={'md'}
+        size={'md'}
+        onClick={() => navigate('/specializations')}
+      >
+        Powrót do strony wybour specjalizacji
+      </Button>
       <AppointmentStepper activeStep={0} />
     </Flex>
   );

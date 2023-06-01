1831404072b69369b539c7746aea50826d789e7d
ArkadiuszWitasik <arkadiuszwitasik.kontakt@gmail.com>
2023-06-01 18:44:20 +0200

    Add new icons and routes in SideMenu links

--- src/helpers/sideMenuLinks.ts
+++ src/helpers/sideMenuLinks.ts
@@ -1,26 +1,51 @@
-import { IconBook, IconHome2, IconNotes, IconNurse } from '@tabler/icons-react';
+import {
+  IconArchive,
+  IconAt,
+  IconBook,
+  IconDeviceDesktopAnalytics,
+  IconHome2,
+  IconNotes,
+  IconNurse,
+  IconUser,
+} from '@tabler/icons-react';
 
 export const NoLoginMenuLinks = [
   { icon: IconHome2, label: 'Strona główna', link: '/' },
   { icon: IconNurse, label: 'Specjaliści', link: '/specializations' },
+  { icon: IconAt, label: 'O nas', link: '/' },
 ];
 
 export const PatientMenuLinks = [
   { icon: IconHome2, label: 'Strona główna', link: '/' },
   { icon: IconNurse, label: 'Specjaliści', link: '/specializations' },
+  { icon: IconAt, label: 'O nas', link: '/' },
   { icon: IconBook, label: 'Wizyty', link: '/visits' },
   { icon: IconNotes, label: 'Historia wizyt', link: '/history' },
 ];
 
 export const DoctorMenuLinks = [
   { icon: IconHome2, label: 'Strona główna', link: '/' },
-  { icon: IconBook, label: 'Wizyty na dany dzień', link: '/' },
-  { icon: IconBook, label: 'Historia wizyt', link: '/' },
+  { icon: IconAt, label: 'O nas', link: '/' },
+  {
+    icon: IconUser,
+    label: 'Profil doktora',
+    link: `/doctor-profile/${sessionStorage.getItem('doctorId')}`,
+  },
+  {
+    icon: IconDeviceDesktopAnalytics,
+    label: 'Panel recepcji',
+    link: '/reception',
+  },
+  { icon: IconArchive, label: 'Archiwum', link: '/archives' },
 ];
 
 export const ReceptionistMenuLinks = [
   { icon: IconHome2, label: 'Strona główna', link: '/' },
-  { icon: IconBook, label: 'Nadchodzące wizyty', link: '/' },
-  { icon: IconBook, label: 'Wizyty na dany dzień', link: '/' },
-  { icon: IconBook, label: 'Historia wizyt', link: '/' },
+  { icon: IconAt, label: 'O nas', link: '/' },
+  {
+    icon: IconDeviceDesktopAnalytics,
+    label: 'Profil recepcji',
+    link: '/reception',
+  },
+  { icon: IconArchive, label: 'Archiwum', link: '/archives' },
 ];
